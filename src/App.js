fc0cd0e1118587feef0021c18b3eab868c2ad4a8
damipoc <damian7DP@hotmail.com>
2022-08-30 15:12:16 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const address = {
+    name: "Name",
+    address: "112 Street",
+    zip: "AA1 BB2"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello {address.name}, your address is {address.address} and zipcode is {address.zip}</p>
     </div>
   );
 }
