fab984ddc75010df03ccf90080e93e41d783b4ca
damipoc <damian7DP@hotmail.com>
2022-08-31 09:20:03 +0100

    Added footer

---
+++ src/footer.jsx
@@ -0,0 +1,10 @@
+const footer = () => {
+    return (
+        <footer>
+            <p>Made by Damian Poclitar</p>
+            <p><a href="mailto:poclitardamian@gmail.com">poclitardamian@gmail.com</a></p>
+        </footer>
+    );
+}
+
+export default footer;
